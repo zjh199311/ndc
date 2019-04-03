f3203f75cadb50dd21f6e6ac19580b3e1cc9bd7f
xuchao001 <18637662571@163.com>
2019-04-03 16:28:25 +0800

    判断商户状态接口

--- dubbo-api/src/main/java/com/zhongjian/service/hm/shopown/HmShopownService.java
+++ dubbo-api/src/main/java/com/zhongjian/service/hm/shopown/HmShopownService.java
@@ -1,6 +1,7 @@
 package com.zhongjian.service.hm.shopown;
 
 import com.zhongjian.dto.common.ResultDTO;
+import com.zhongjian.dto.hm.shopown.query.HmShopownStatusQueryDTO;
 import com.zhongjian.dto.hm.shopown.result.HmShopownResultDTO;
 
 /**
@@ -12,4 +13,11 @@ public interface HmShopownService {
      * 购物车显示列表
      */
     ResultDTO<HmShopownResultDTO> queryList(String loginToken);
+    
+    /**
+     * 判断所有商铺是不是指定状态
+     * @param hmShopownStatusQueryDTO
+     * @return
+     */
+    ResultDTO<Boolean> judgeHmShopownStatus(HmShopownStatusQueryDTO hmShopownStatusQueryDTO);
 }
---
+++ dubbo-server-order/src/main/java/com/zhongjian/service/hm/shopown/impl/HmShopownServiceImpl.java
@@ -0,0 +1,70 @@
+package com.zhongjian.service.hm.shopown.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.zhongjian.common.constant.enums.hm.shopown.HmShopownEnum;
+import com.zhongjian.dao.entity.hm.shopown.HmShopownBean;
+import com.zhongjian.dao.framework.impl.HmBaseService;
+import com.zhongjian.dto.common.ResultDTO;
+import com.zhongjian.dto.hm.shopown.query.HmShopownStatusQueryDTO;
+import com.zhongjian.dto.hm.shopown.result.HmShopownResultDTO;
+import com.zhongjian.service.hm.shopown.HmShopownService;
+
+@Service("hmShopownService")
+public class HmShopownServiceImpl extends HmBaseService<HmShopownBean, Integer>
+        implements HmShopownService {
+
+    @Override
+    public ResultDTO<HmShopownResultDTO> queryList(String loginToken) {
+        return null;
+    }
+
+    @Override
+    public ResultDTO<Boolean> judgeHmShopownStatus(
+            HmShopownStatusQueryDTO hmShopownStatusQueryDTO) {
+        ResultDTO<Boolean> resultDTO = new ResultDTO<Boolean>();
+        resultDTO.setFlag(false);
+        if (null == hmShopownStatusQueryDTO.getPids()
+                || 0 == hmShopownStatusQueryDTO.getPids().size()) {
+            resultDTO.setErrorMessage(HmShopownEnum.PID_IS_NULL.getMsg());
+            resultDTO.setStatusCode(HmShopownEnum.PID_IS_NULL.getCode());
+            return resultDTO;
+        }
+        if (null == hmShopownStatusQueryDTO.getStatus()) {
+            resultDTO.setErrorMessage(HmShopownEnum.STATUS_IS_NULL.getMsg());
+            resultDTO.setStatusCode(HmShopownEnum.STATUS_IS_NULL.getCode());
+            return resultDTO;
+        }
+        List<HmShopownBean> hmShopownBeans = this.dao.executeListMethod(
+                hmShopownStatusQueryDTO, "selectHmShopownStatusByPids",
+                HmShopownBean.class);
+        // 默认返回状态匹配
+        resultDTO.setFlag(true);
+        resultDTO.setData(true);
+        for (HmShopownBean hmShopownBean : hmShopownBeans) {
+            // 如果商户状态与传入状态不匹配，返回false
+            if (!hmShopownStatusQueryDTO.getStatus().equals(
+                    hmShopownBean.getStatus())) {
+                resultDTO.setData(false);
+                break;
+            }
+            // 如果店铺状态为预约中，但是该店铺没有开启预约 返回false
+            if (2 == hmShopownBean.getStatus()
+                    && 0 == hmShopownBean.getIsAppointment()) {
+                resultDTO.setData(false);
+                break;
+            }
+            //如果店铺状态为打烊或开张并且店铺为开启预约。返回false
+            if ((1 == hmShopownBean.getStatus() || 0 == hmShopownBean
+                    .getStatus()) && 1 == hmShopownBean.getIsAppointment()) {
+                resultDTO.setData(false);
+                break;
+            }
+        }
+        return resultDTO;
+    }
+
+}
