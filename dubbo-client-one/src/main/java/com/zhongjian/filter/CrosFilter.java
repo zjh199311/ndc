ee862ca276a60569d52baae336c0dd689394084b
chendi <15395068264@163.com>
2019-04-04 14:11:43 +0800

    commit dubbo-client-one

--- dubbo-client-one/src/main/java/com/zhongjian/filter/CrosFilter.java
+++ dubbo-client-one/src/main/java/com/zhongjian/filter/CrosFilter.java
@@ -10,12 +10,23 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
+import com.zhongjian.common.GsonUtil;
+import com.zhongjian.common.ResponseHandle;
+import com.zhongjian.common.SpringContextHolder;
+import com.zhongjian.component.PropUtil;
+import com.zhongjian.dto.common.CommonMessageEnum;
+import com.zhongjian.dto.common.ResultUtil;
+import com.zhongjian.service.hm.basket.HmBasketService;
+import com.zhongjian.service.user.UserService;
+import com.zhongjian.util.CheckSumBuilderUtil;
+
 /**
  * Servlet Filter implementation class CrosFilter
  */
 @WebFilter(value = "/*", asyncSupported = true)
 public class CrosFilter implements Filter {
 
+	private PropUtil propUtil = (PropUtil) SpringContextHolder.getBean(PropUtil.class);
     /**
      * Default constructor. 
      */
@@ -32,11 +43,32 @@ public class CrosFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if (!propUtil.getIsDebug()) {
+			//check sign
+			String appKey = request.getParameter("appKey");
+			String curTime = request.getParameter("curTime");
+			String nonce = request.getParameter("nonce");
+			String checkSum = request.getParameter("checkSum");
+			String realAppKey = propUtil.getAppkey();
+			String realCheckSum = CheckSumBuilderUtil.getCheckSum(propUtil.getAppSecret(), nonce, curTime);
+			if (appKey == null || !appKey.equals(realAppKey)) {
+				ResponseHandle.wrappedResponse(response, GsonUtil.GsonString(ResultUtil.getFail(CommonMessageEnum.NO_PERMISION)));
+			}
+			if( checkSum == null || !checkSum.equals(realCheckSum)) {
+				ResponseHandle.wrappedResponse(response, GsonUtil.GsonString(ResultUtil.getFail(CommonMessageEnum.NO_PERMISION)));
+			}
+		}
+		
 		HttpServletResponse httpResponse = (HttpServletResponse)response;
 		httpResponse.setHeader("Access-Control-Allow-Origin", "*");
 		httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
 		httpResponse.setHeader("Access-Control-Allow-Methods", "*");
-		chain.doFilter(request, httpResponse);
+		//add login_token jungle
+		if (request.getParameter("login_token")  != null) {
+			
+//			request.setAttribute("uid",userService.getUidByLoginToken(request.getParameter("login_token")));
+			}
+		chain.doFilter(request, response);
 	}
 
 	/**
