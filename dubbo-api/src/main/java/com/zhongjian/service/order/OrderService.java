2cf8816fd8492a975c3fc3536ff5ef0119fa89ea
chendi <15395068264@163.com>
2019-04-10 15:51:10 +0800

    commit by chendi for orderservice

--- dubbo-api/src/main/java/com/zhongjian/service/order/OrderService.java
+++ dubbo-api/src/main/java/com/zhongjian/service/order/OrderService.java
@@ -1,5 +1,7 @@
 package com.zhongjian.service.order;
 
+import java.util.Map;
+
 import com.zhongjian.dto.common.ResultDTO;
 import com.zhongjian.dto.order.reult.PreviewOrderResultDTO;
 
@@ -7,5 +9,10 @@ public interface OrderService {
 
 	//type 0选用积分 extra=null 1选用优惠券 extra = couponId 
 	ResultDTO<PreviewOrderResultDTO> previewOrder(Integer uid,Integer sids[],Integer isAppointment,String type,String extra,String isSelfMention);
+	
+	//0 "16:20"  1 "04-11 08:48"
+	String previewOrderTime(Integer isAppointment);
+
+	Map<String,String> previewOrderAddress(Integer id,Integer uid);
 	
 }
