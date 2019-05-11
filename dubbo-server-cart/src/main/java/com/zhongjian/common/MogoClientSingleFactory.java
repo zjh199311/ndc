b119af758f34b6974b5cbfe994e9b9000430394b
chendi <15395068264@163.com>
2019-05-11 16:19:20 +0800

    commit

---
+++ dubbo-server-cart/src/main/java/com/zhongjian/common/MogoClientSingleFactory.java
@@ -0,0 +1,48 @@
+package com.zhongjian.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.zhongjian.commoncomponent.PropUtil;
+
+/*
+ * mongodb数据库链接池
+ */
+public class MogoClientSingleFactory {
+	
+	private static MongoClient mongoClient = null;
+
+	public static MongoClient getMongoClient() {
+		if (mongoClient == null) {
+			synchronized (MogoClientSingleFactory.class) {
+				if (mongoClient == null) {
+					MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
+					builder.connectionsPerHost(50);// 与目标数据库可以建立的最大链接数
+					builder.minConnectionsPerHost(5);
+					builder.connectTimeout(1000 * 60 * 20);// 与数据库建立链接的超时时间
+					builder.maxWaitTime(5000);// 一个线程成功获取到一个可用数据库之前的最大等待时间
+					builder.threadsAllowedToBlockForConnectionMultiplier(100);
+					builder.maxConnectionIdleTime(0);
+					builder.maxConnectionLifeTime(0);
+					builder.threadsAllowedToBlockForConnectionMultiplier(5);
+					builder.socketTimeout(0);
+					MongoClientOptions myOptions = builder.build();
+					PropUtil propUtil = (PropUtil) SpringContextHolder.getBean(PropUtil.class);
+					if (StringUtils.isBlank(propUtil.getMongoPassword()) || StringUtils.isBlank(propUtil.getMongoUserName())) {
+						mongoClient = new MongoClient(new ServerAddress(propUtil.getMongoIp(), propUtil.getMongoPort()), myOptions);
+					} else {
+						MongoCredential credential = MongoCredential.createCredential(propUtil.getMongoUserName(),
+								propUtil.getMongodbName(), propUtil.getMongoPassword().toCharArray());
+						mongoClient = new MongoClient(new ServerAddress(propUtil.getMongoIp(), propUtil.getMongoPort()), credential,
+								myOptions);
+					}
+				}
+			}
+		}
+		return mongoClient;
+		
+	}
+}
--- dubbo-server-cart/src/main/java/com/zhongjian/dao/MongoDBDaoBase.java
+++ dubbo-server-cart/src/main/java/com/zhongjian/dao/MongoDBDaoBase.java
@@ -2,26 +2,24 @@ package com.zhongjian.dao;
 
 import org.bson.Document;
 
-import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.zhongjian.common.mogoClientSingleFactory;
+import com.zhongjian.common.MogoClientSingleFactory;
 
 /*
  * mongodb数据库链接池
  */
 public class MongoDBDaoBase
 {
-    protected MongoClient mongoClient = mogoClientSingleFactory.mongoClient;
 
     protected MongoDatabase getDb(String dbName)
     {
-        return mongoClient.getDatabase(dbName);
+        return MogoClientSingleFactory.getMongoClient().getDatabase(dbName);
     }
 
     protected MongoCollection<Document> getCollection(String dbName, String collectionName)
     {
-    	MongoDatabase db = mongoClient.getDatabase(dbName);
+    	MongoDatabase db = MogoClientSingleFactory.getMongoClient().getDatabase(dbName);
     	MongoCollection<Document> collection = db.getCollection(collectionName);
     	return collection;
     }
