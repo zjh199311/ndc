b0577806c436fd58d80f3fff7a51a1084d7bfbfd
chendi <15395068264@163.com>
2019-05-13 16:26:33 +0800

    commit

--- dubbo-server-order/src/main/java/com/zhongjian/dao/jdbctemplate/IntegralVipDao.java
+++ dubbo-server-order/src/main/java/com/zhongjian/dao/jdbctemplate/IntegralVipDao.java
@@ -1,14 +1,26 @@
 package com.zhongjian.dao.jdbctemplate;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.zhongjian.dao.MongoDBDaoBase;
+
 @Repository
-public class IntegralVipDao {
+public class IntegralVipDao extends MongoDBDaoBase{
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -48,5 +60,61 @@ public class IntegralVipDao {
 	public void addIntegralLog(Integer uid, Integer integral , Integer type,Integer ctime) {
 		String sql = "INSERT INTO `hm_integral_log` (integral,uid,type,ctime) VALUES (?, ?, ?, ?)";
 		jdbcTemplate.update(sql,integral,uid,type,ctime);
+	}
+	
+	public Map<String, Object> getDefualtVipConfig() {
+		MongoCollection<Document> collection = getCollection("nidcai", "vipSetting");
+		Document filter = new Document();
+		filter.append("effectTime","year");
+		filter.append("enabled",true);
+		List<Document> results = new ArrayList<Document>();
+		FindIterable<Document> iterables = collection.find(filter);
+		MongoCursor<Document> cursor = iterables.iterator();
+		while (cursor.hasNext()) {
+			results.add(cursor.next());
+		}
+		HashMap<String, Object> resMap = new HashMap<String, Object>();
+		for (Iterator<Document> iterator = results.iterator(); iterator.hasNext();) {
+			Document document = iterator.next();
+			resMap.put("discount",document.get("discount"));
+			resMap.put("riderDiscount",document.get("riderDiscount"));
+			Object limitDayReliefObj = document.get("limitDayRelief");
+			Object limitOneObj = document.get("limitOne");
+			if (limitOneObj!=null && limitOneObj instanceof String) {
+				resMap.put("limitOne",Double.valueOf((String) limitOneObj));
+			}else if (limitOneObj!=null && limitOneObj instanceof Double){
+				resMap.put("limitOne",Double.valueOf((Double) limitOneObj));
+			}
+			if (limitDayReliefObj!=null && limitDayReliefObj instanceof String) {
+				resMap.put("limitDayRelief",Double.valueOf((String) limitDayReliefObj));
+			}else if (limitDayReliefObj!=null && limitDayReliefObj instanceof Double){
+				resMap.put("limitDayRelief",Double.valueOf((Double) limitDayReliefObj));
+			}
+			break;
+		}
+		return resMap;
+	}
+	
+	public Map<String, Object> getVipConfigByUid() {
+		MongoCollection<Document> collection = getCollection("nidcai", "vipSetting");
+		Document filter = new Document();
+		filter.append("effectTime","year");
+		filter.append("enabled",true);
+		List<Document> results = new ArrayList<Document>();
+		FindIterable<Document> iterables = collection.find(filter);
+		MongoCursor<Document> cursor = iterables.iterator();
+		while (cursor.hasNext()) {
+			results.add(cursor.next());
+		}
+		HashMap<String, Object> resMap = new HashMap<String, Object>();
+		for (Iterator<Document> iterator = results.iterator(); iterator.hasNext();) {
+			Document document = iterator.next();
+			resMap.put("discount",document.get("discount"));
+			resMap.put("riderDiscount",document.get("riderDiscount"));
+			resMap.put("limitDayRelief",document.get("limitDayRelief"));
+			resMap.put("limitOne",document.get("limitOne"));
+			break;
+		}
+		return resMap;
 	}
 }
