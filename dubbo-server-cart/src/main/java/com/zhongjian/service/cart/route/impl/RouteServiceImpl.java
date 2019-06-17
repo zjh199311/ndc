44fdda71d61f2c30b75f01586e508003d0ca1be9
754224613 <754224613@qq.com>
2019-06-17 10:58:04 +0800

    删除数组中为0的数字

--- dubbo-server-cart/src/main/java/com/zhongjian/service/cart/route/impl/RouteServiceImpl.java
+++ dubbo-server-cart/src/main/java/com/zhongjian/service/cart/route/impl/RouteServiceImpl.java
@@ -4,13 +4,8 @@ import com.zhongjian.common.constant.FinalDatas;
 import com.zhongjian.dao.entity.cart.shopown.CartShopownBean;
 import com.zhongjian.dao.framework.impl.HmBaseService;
 import com.zhongjian.dto.cart.route.BasketCVSidDTO;
-import com.zhongjian.dto.common.CommonMessageEnum;
-import com.zhongjian.dto.common.ResultUtil;
 import com.zhongjian.service.cart.route.RouteService;
-import lombok.Data;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 /**
  * @Author: ldd
@@ -34,17 +29,36 @@ public class RouteServiceImpl extends HmBaseService<CartShopownBean, Integer> im
                 cvSids[i] = sids[i];
             }
         }
-        if (basketSids[basketSids.length - 1] == 0) {
-            basketSids = Arrays.copyOf(basketSids, basketSids.length - 1);
-        }
-        if (cvSids[cvSids.length - 1] == 0) {
-            cvSids = Arrays.copyOf(cvSids, cvSids.length - 1);
-        }
-        basketCVSidDTO.setBasketSids(basketSids);
-        basketCVSidDTO.setCvSids(cvSids);
+
+        basketCVSidDTO.setBasketSids(remove(basketSids));
+        basketCVSidDTO.setCvSids(remove(cvSids));
 
         return basketCVSidDTO;
     }
 
+    public static int[] remove(int[] sid) {
+        // 定义数组
+        int h = 0; // 设置一个变量作为增量
+        // 循环读取oldArr数组的值
+        for (int b : sid) {
+            // 判断，如果oldArr数组的值不为0那么h就加1
+            if (b != 0) {
+                h++;
+            }
+        }
+        // 得到了数组里不为0的个数，以此个数定义一个新数组，长度就是h
+        int newArr[] = new int[h];
+        // 这里偷个懒，不想从新定义增量了，所以把增量的值改为0
+        h = 0;
+        // 在次循环读取oldArr数组的值
+        for (int c : sid) {
+            // 把不为0的值写入到newArr数组里面
+            if (c != 0) {
+                newArr[h] = c;
+                h++;// h作为newArr数组的下标，没写如一个值，下标h加1
+            }
+        }
+        return newArr;
+    }
 }
 
